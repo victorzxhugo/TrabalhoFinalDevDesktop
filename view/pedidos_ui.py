155d16a635ea437f2931d9dedb4a757950395931
Joao Victor Crispim <jvictorcrispim@gmail.com>
2023-12-05 19:08:24 -0300

    pedidos corrigidos

--- view/pedidos_ui.py
+++ view/pedidos_ui.py
@@ -37,9 +37,7 @@ class Ui_janela_pedidos(object):
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.widget_pedidos_maior = QWidget(janela_pedidos)
         self.widget_pedidos_maior.setObjectName(u"widget_pedidos_maior")
-        self.horizontalLayout_4 = QHBoxLayout(self.widget_pedidos_maior)
-        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3 = QHBoxLayout(self.widget_pedidos_maior)
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
         self.verticalLayout_2 = QVBoxLayout()
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
@@ -47,6 +45,16 @@ class Ui_janela_pedidos(object):
         self.lbl_pedidos_nome_criar_pedidos.setObjectName(u"lbl_pedidos_nome_criar_pedidos")
 
         self.verticalLayout_2.addWidget(self.lbl_pedidos_nome_criar_pedidos)
+
+        self.lbl_pedidos_identificador = QLabel(self.widget_pedidos_maior)
+        self.lbl_pedidos_identificador.setObjectName(u"lbl_pedidos_identificador")
+
+        self.verticalLayout_2.addWidget(self.lbl_pedidos_identificador)
+
+        self.txt_pedidos_identificador = QLineEdit(self.widget_pedidos_maior)
+        self.txt_pedidos_identificador.setObjectName(u"txt_pedidos_identificador")
+
+        self.verticalLayout_2.addWidget(self.txt_pedidos_identificador)
 
         self.lbl_pedidos_nome_produto = QLabel(self.widget_pedidos_maior)
         self.lbl_pedidos_nome_produto.setObjectName(u"lbl_pedidos_nome_produto")
@@ -98,7 +106,9 @@ class Ui_janela_pedidos(object):
 
         self.widget_pedidos_menor = QWidget(self.widget_pedidos_maior)
         self.widget_pedidos_menor.setObjectName(u"widget_pedidos_menor")
-        self.verticalLayout_3 = QVBoxLayout(self.widget_pedidos_menor)
+        self.verticalLayout_4 = QVBoxLayout(self.widget_pedidos_menor)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.verticalLayout_3 = QVBoxLayout()
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
         self.lbl_pedidos_nome_receber_pedidos = QLabel(self.widget_pedidos_menor)
         self.lbl_pedidos_nome_receber_pedidos.setObjectName(u"lbl_pedidos_nome_receber_pedidos")
@@ -110,16 +120,26 @@ class Ui_janela_pedidos(object):
 
         self.verticalLayout_3.addWidget(self.tb_widget_pedidos_receber_pedidos)
 
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.btn_pedidos_visualizar = QPushButton(self.widget_pedidos_menor)
+        self.btn_pedidos_visualizar.setObjectName(u"btn_pedidos_visualizar")
+
+        self.horizontalLayout_4.addWidget(self.btn_pedidos_visualizar)
+
         self.btn_pedidos_receber = QPushButton(self.widget_pedidos_menor)
         self.btn_pedidos_receber.setObjectName(u"btn_pedidos_receber")
 
-        self.verticalLayout_3.addWidget(self.btn_pedidos_receber)
+        self.horizontalLayout_4.addWidget(self.btn_pedidos_receber)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_4)
+
+
+        self.verticalLayout_4.addLayout(self.verticalLayout_3)
 
 
         self.horizontalLayout_3.addWidget(self.widget_pedidos_menor)
-
-
-        self.horizontalLayout_4.addLayout(self.horizontalLayout_3)
 
 
         self.horizontalLayout.addWidget(self.widget_pedidos_maior)
@@ -137,12 +157,14 @@ class Ui_janela_pedidos(object):
         janela_pedidos.setWindowTitle(QCoreApplication.translate("janela_pedidos", u"Dialog", None))
         self.lbl_pedidos_nome_pedidos.setText(QCoreApplication.translate("janela_pedidos", u"<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; font-weight:600;\">Pedidos</span></p></body></html>", None))
         self.lbl_pedidos_nome_criar_pedidos.setText(QCoreApplication.translate("janela_pedidos", u"Criar pedidos", None))
+        self.lbl_pedidos_identificador.setText(QCoreApplication.translate("janela_pedidos", u"Identificador do pedido", None))
         self.lbl_pedidos_nome_produto.setText(QCoreApplication.translate("janela_pedidos", u"Produto", None))
         self.btn_pedidos_buscar_produto.setText(QCoreApplication.translate("janela_pedidos", u"Buscar", None))
         self.lbl_pedidos_nome_quantidade.setText(QCoreApplication.translate("janela_pedidos", u"Quantidade", None))
         self.btn_pedidos_adicionar.setText(QCoreApplication.translate("janela_pedidos", u"Adicionar", None))
         self.btn_pedidos_finalizar_pedidos.setText(QCoreApplication.translate("janela_pedidos", u"Finalizar pedido", None))
         self.lbl_pedidos_nome_receber_pedidos.setText(QCoreApplication.translate("janela_pedidos", u"Receber pedidos", None))
+        self.btn_pedidos_visualizar.setText(QCoreApplication.translate("janela_pedidos", u"Visualizar", None))
         self.btn_pedidos_receber.setText(QCoreApplication.translate("janela_pedidos", u"Receber", None))
     # retranslateUi
 
