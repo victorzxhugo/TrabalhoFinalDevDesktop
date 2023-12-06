334787f120a6a42dd1b3642a25d4b9800a0644bb
Victor Hugo <victorzxhugoo@gmail.com>
2023-12-05 21:15:09 -0300

    MUITA COISA MEU PARCEIRO

--- infra/entities/produto.py
+++ infra/entities/produto.py
@@ -11,9 +11,11 @@ class Produto (Base):
     id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
     nome: Mapped[str] = mapped_column(nullable=False)
     preco: Mapped[float] = mapped_column(nullable=False)
-    quantidade: Mapped[int] = mapped_column(nullable=False)
-    categoria_id: Mapped[int] = mapped_column(ForeignKey("categoria.id", ondelete="CASCADE"))
-    pedido_id: Mapped[int] = mapped_column(ForeignKey("pedido.id", ondelete="CASCADE"))
+    quantidade: Mapped[int] = mapped_column(nullable=True)
+    categoria: Mapped[str] = mapped_column(nullable=False)
+
+
+
 
     def __repr__(self):
         return f'Produto [nome={self.nome}, preco={self.preco}, quantidade_em_estoque={self.quantidade_em_estoque}]'
--- infra/service/produto_service.py
+++ infra/service/produto_service.py
@@ -3,20 +3,21 @@ from PySide6.QtWidgets import QMessageBox
 from infra.entities.produto import Produto
 from infra.repository.produto_repository import ProdutoRepository
 from infra.repository.pedidoFornecedor_repository import PedidoFornecedorRepository
-from infra.service.cadastro_window_service import MainWindowService
+
 
 
 class ProdutoService:
-    def insert_Produto(self, cadastro_window):
+    def insert_Produto(self, cadastro_ui):
         produto = Produto()
-        produto.nome = cadastro_window.txt_cadastro_nome.text()
-        produto.preco = cadastro_window.txt_cadastro_preco.text()
-        produto.categoria_id = cadastro_window.selectedRow()
+        produto.nome = cadastro_ui.txt_cadastro_nome.text()
+        produto.preco = cadastro_ui.txt_cadastro_preco.text()
+        produto.categoria_id = cadastro_ui.selectedRow()
 
         try:
             self.produto_repository.insert_one_produto(produto)
-            cadastro_window.text_nome_produto.setText('')
-            self.service_cadastro_window.populate_table_produto(cadastro_window)
-            QMessageBox.information(cadastro_window, 'produtos', f'produto cadastrado com sucesso')
+            cadastro_ui.txt_cadastro_nome.setText('')
+            cadastro_ui.txt_cadastro_preco.setText('')
+            self.service_cadastro_ui.populate_table_produto(cadastro_ui)
+            QMessageBox.information(cadastro_ui, 'produtos', f'produto cadastrado com sucesso')
         except Exception as e:
-            QMessageBox.warning(cadastro_window, 'produtos', f'Erro ao cadastrar produto. \nErro: {e}')
+            QMessageBox.warning(cadastro_ui, 'produtos', f'Erro ao cadastrar produto. \nErro: {e}')
